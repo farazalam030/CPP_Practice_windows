06a83ea93c98b194154a3262a1101219a5db3853
farazalam030 <farazalam030@gmail.com>
2022-07-28 07:24:29 +0530

    iInitial Commit 1

---
+++ src/CPP_config.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : CPP_config.cpp
+// Author      : Faraz Alam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  int a = 10;
+  vector<int> b{1, 2, 3, 4, 5, 6};
+
+  for (auto i : b) {
+    cout << i << ", ";
+  }
+  cout << endl;
+  return 0;
+
+  /*
+   *
+   * jkherswdgiufgweukjnlkjsdbhgvsdjlkjdsfbghvsw
+   */
+}
